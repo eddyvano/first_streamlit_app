f544cd4c09492d162fed39f867ee1d7be61d9a66
eddyvano <128822018+eddyvano@users.noreply.github.com>
2023-03-31 12:31:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,6 @@ streamlit.header("The fruit loadlist contains:")
 streamlit.dataframe(my_data_rows)
 
 #allow end-user to add a fruit to the list
-add_my_fruit = streamlit.multiselect("What fruit would you like to add", list(my_fruit_list.index),['Avocado','Strawberries'])
+add_my_fruit = streamlit.multiselect("What fruit would you like to add", list(my_fruit_list.index),[])
 fruits_to_show_2 = my_fruit_list.loc[add_my_fruit]
+streamlit.write('Thanks for adding', add_my_fruit)
